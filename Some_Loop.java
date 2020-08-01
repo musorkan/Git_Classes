a45f8b1dce869ea330aa71d65ca10c4fd48cb569
musorkan <musorkan@gmail.com>
2020-08-01 12:47:41 -0700

    change 1

--- Some_Loop.java
+++ Some_Loop.java
@@ -1,12 +1,9 @@
 class Some_Loop{
 public static void main(String[] args){
 System.out.println("Hey Body!");
-<<<<<<< HEAD
 
     System.out.println("Hi Guys!");
-=======
-  System.out.println("Hey what's up!")
->>>>>>> 40be868334c382b9421f148844f1390cbd6dbef5
+  System.out.println("Hey what's up!");
 }
 
 }
